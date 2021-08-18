67de1d4a28678546a1ba1a060fac4422b578ba01
jiazhaoquan <jzq17856170605@163.com>
2021-08-18 18:42:07 +0800

    推送push

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,12 @@
 public class test {
 //    测试第一次建立仓库
     /*分享仓库给github
+    创建在github上的仓库
 
+    步骤：1：VCS下面的import into Version Control
+    下面的Share Project on GitHub
+    2:将当前的push到本地库到远程库
+    git-》+add-》commit Directory
     * */
 
 }
